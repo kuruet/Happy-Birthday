f5fb5ade5de7f95f577570b54f64b75402b7be1d
kuruet <bpratik728@gmail.com>
2025-04-03 11:03:18 +0530

    Fixed base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Happy-Birthday/', // Set the base path to match your repository name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
